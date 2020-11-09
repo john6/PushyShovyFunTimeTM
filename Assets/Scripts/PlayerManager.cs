78094beb1da5b72c3c026e509dfaea8a01ada565
john6 <johnnyrussell6@gmail.com>
2020-11-09 14:38:58 -0800

    putting the tutorial stuff in its own branch to make way for new physics stuff in master

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,6 +3,8 @@ using UnityEngine.EventSystems;
 using Manager;
 
 using Photon.Pun;
+using Photon.Pun.Demo.PunBasics;
+//TODO CameraWork comes from PunBasics, maybe should be removed/changed later
 
 using System.Collections;
 
@@ -11,21 +13,57 @@ using System.Collections;
 /// Player manager.
 /// Handles pushing and shoving.
 /// </summary>
-public class PlayerManager : MonoBehaviourPunCallbacks
+public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
 {
+    #region IPunObservable implementation
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(IsPushing);
+            stream.SendNext(Health);
+        }
+        else
+        {
+            // Network player, receive data
+            this.IsPushing = (bool)stream.ReceiveNext();
+            this.Health = (float)stream.ReceiveNext();
+        }
+    }
+
+
+    #endregion
+
+
     #region Private Fields
 
     [Tooltip("The trigger volume from which you can push another player")]
     [SerializeField]
     private GameObject pushVolume;
+    
     //True, when the user is pushing
     bool IsPushing;
+
+    [Tooltip("The Following this player")]
+    [SerializeField]
+    CameraWork _cameraWork;
     #endregion
+
 
     #region Public Fields
 
     [Tooltip("The current Health of our player")]
     public float Health = 1f;
+
+    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+    public static GameObject LocalPlayerInstance;
+
+    [Tooltip("The Player's UI GameObject Prefab")]
+    [SerializeField]
+    public GameObject PlayerUiPrefab;
 
     #endregion
 
@@ -36,6 +74,16 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     /// </summary>
     void Awake()
     {
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+        if (photonView.IsMine)
+        {
+            PlayerManager.LocalPlayerInstance = this.gameObject;
+        }
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(this.gameObject);
+
         if (pushVolume == null)
         {
             Debug.LogError("<Color=Red><a>Missing</a></Color> pushVolume Reference.", this);
@@ -46,6 +94,38 @@ public class PlayerManager : MonoBehaviourPunCallbacks
         }
     }
 
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+    /// </summary>
+    void Start()
+    {
+        CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+        if (_cameraWork != null)
+        {
+            if (photonView.IsMine)
+            {
+                _cameraWork.OnStartFollowing();
+            }
+        }
+        else
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+        }
+
+        if (PlayerUiPrefab != null)
+        {
+            GameObject _uiGo = Instantiate(PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+        }
+        else
+        {
+            Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+        }
+
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     /// <summary>
     /// MonoBehaviour method called on GameObject by Unity on every frame.
@@ -53,7 +133,10 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     void Update()
     {
 
-        ProcessInputs();
+        if (photonView.IsMine)
+        {
+            ProcessInputs();
+        }
 
         // trigger pushing active state
         if (pushVolume != null && IsPushing != pushVolume.activeInHierarchy)
@@ -63,7 +146,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
         if (Health <= 0f)
         {
-            GameManager.Instance.LeaveRoom();
+            Manager.GameManager.Instance.LeaveRoom();
         }
     }
 
@@ -109,7 +192,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
     #endregion
 
-    #region Custom
+    #region Private Methods
 
     /// <summary>
     /// Processes the inputs. Maintain a flag representing when the user is pressing Fire.
@@ -132,5 +215,26 @@ public class PlayerManager : MonoBehaviourPunCallbacks
         }
     }
 
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+    {
+        this.CalledOnLevelWasLoaded(scene.buildIndex);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        this.CalledOnLevelWasLoaded(level);
+    }
+
+    void CalledOnLevelWasLoaded(int level)
+    {
+        // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+        //Dont need to do this since we aren't reloading levels but I'm putting the callback in just in case
+        //if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+        //{
+        //    transform.position = new Vector3(0f, 5f, 0f);
+        //}
+        GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+        _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+    }
     #endregion
 }
